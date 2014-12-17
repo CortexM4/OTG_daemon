dd00b8e9fa6d5aad1bcbabfb5235abf0ad50db03
CortexM4 <anttimoshenko@outlook.com>
2014-12-17 00:54:46 -0800

    Запись в конфиг теперь не через sed-скрипт, а средствами JAVA. Работает WLAN, а также TRYC для рестарта сетей.

--- src/otgservice/Connector.java
+++ src/otgservice/Connector.java
@@ -7,6 +7,9 @@ package otgservice;
 
 import USB_OTG.Serial;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -21,6 +24,7 @@ import otgservice.Interface.*;
 public class Connector implements IObserver {
 
     private IObservable connector;
+    private final String file = "/etc/network/interfaces";
 
     public Connector() {
         connector = new Serial(this);
@@ -32,6 +36,8 @@ public class Connector implements IObserver {
 
     @Override
     public void update(String data) {
+
+        /* Получение списка доступных сетей */
         if (data.regionMatches(0, "GETE", 0, 4)) {
 
             String[] cmd_search = {
@@ -59,33 +65,55 @@ public class Connector implements IObserver {
             }
         }
 
+        /* Запись конфигурации в /etc/network/interfaces */
         if (data.regionMatches(0, "WLAN", 0, 4)) {
+            data = data.replaceAll("\n", "");                       // Хер знает откуда там перенос строки, но ИНОГДА появляется
             int index_net = data.indexOf(" ", 0);
             int index_pwd = data.indexOf(" ", index_net + 1);
             String essid = data.substring(index_net + 1, index_pwd);
             String pwd = data.substring(index_pwd + 1);
 
-            System.out.println("Trying restart network: "+essid+" "+pwd);
-            String[] cmd_wlan = {
-                "/bin/sh",
-                "-c",
-                "sed -i -e 's/wpa-ssid .*/wpa-ssid \"" + essid + "\"/' -e 's/wpa-psk .*/wpa-psk \"" + pwd + "\"/'  "
-                    + "/etc/network/interfaces"
-            };
+            System.out.println("Network param: " + essid + " " + pwd);
 
+            /* Чтение файла конфигурации сети и внесение новых данных */
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(
+                        new FileInputStream(file)));
+                String tmp = null; // буфер чтения
+                StringBuilder ost = new StringBuilder();
+                while ((tmp = br.readLine()) != null) {
+                    int i = tmp.indexOf("wpa-ssid");
+                    if (tmp.contains("wpa-ssid")) {
+                        ost.append("\twpa-ssid \"").append(essid).append("\"\n");
+                    }
+                    else if (tmp.contains("wpa-psk")) {
+                        ost.append("\twpa-psk \"").append(pwd).append("\"\n");
+                    }
+                    else {
+                        ost.append(tmp).append("\n");
+                    }
+                }
+                br.close();
+                /* Запись измененных данных */
+                FileWriter fw = new FileWriter(new File(file));
+                fw.write(ost.toString());
+                fw.close();
+            }
+            catch (IOException ex) {
+                Logger.getLogger(Connector.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        /* Перезапуск сети */
+        if (data.regionMatches(0, "TRYC", 0, 4)) {
             Runtime runtime = Runtime.getRuntime();
             try {
-                Process ls = runtime.exec(cmd_wlan);
-                //ls = runtime.exec("/etc/init.d/networking restart");
-                //InputStream in = ls.getInputStream();
-
-                //BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-
-                //String line;
-                //while ((line = reader.readLine()) != null) {
-                //    System.out.println(line);
-                //}
-                //ls = runtime.exec("/etc/init.d/networking restart");
+                Process process = runtime.exec("/etc/init.d/networking restart");
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    System.out.println(line);
+                }
             }
             catch (IOException ex) {
                 Logger.getLogger(Connector.class.getName()).log(Level.SEVERE, null, ex);
